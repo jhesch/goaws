be9f4a3e6e879b307704bb935749f3e86a9fd28c
Jacob Hesch <jacob.hesch@gmail.com>
2014-04-17 14:23:03 -0700

    Add support for retrieving resource record sets.

--- route53.go
+++ route53.go
@@ -4,6 +4,7 @@ import (
 	"encoding/xml"
 	"fmt"
 	"log"
+	"os"
 )
 
 type Route53 struct {
@@ -50,6 +51,38 @@ type NameServers struct {
 
 type DelegationSet struct {
 	NameServers NameServers
+}
+
+type ListResourceRecordSetsResponse struct {
+	XMLName            xml.Name `xml:"ListResourceRecordSetsResponse"`
+	ResourceRecordSets []ResourceRecordSets
+	IsTruncated        bool
+	NextRecordName     string
+	NextRecordType     string
+	MaxItems           int
+}
+
+type ResourceRecordSets struct {
+	XMLName           xml.Name `xml:"ResourceRecordSets"`
+	ResourceRecordSet []ResourceRecordSet
+}
+
+type ResourceRecordSet struct {
+	XMLName         xml.Name `xml:"ResourceRecordSet"`
+	Name            string
+	Type            string
+	TTL             string
+	ResourceRecords []ResourceRecords
+}
+
+type ResourceRecords struct {
+	XMLName        xml.Name `xml:"ResourceRecords"`
+	ResourceRecord []ResourceRecord
+}
+
+type ResourceRecord struct {
+	XMLName xml.Name `xml:"ResourceRecord"`
+	Value   string
 }
 
 var route53Endpoint = func() string {
@@ -113,4 +146,44 @@ func (r *Route53) GetHostedZone(id string) GetHostedZoneResponse {
 	}
 
 	return v
+}
+
+// getResourceRecordSetsChunk returns the resource record sets for the
+// given zone starting with the record identified by name.
+func (r *Route53) getResourceRecordSetsChunk(
+	zone string, name string, records []ResourceRecordSet) []ResourceRecordSet {
+
+	url := fmt.Sprintf("%shostedzone/%s/rrset", route53Endpoint, zone)
+	if name != "" {
+		url = fmt.Sprintf("%s?name=%s", url, name)
+	}
+
+	result, err := request(&RequestParams{Url: url, Auth: r.auth})
+
+	v := ListResourceRecordSetsResponse{}
+	err = xml.Unmarshal(result, &v)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %s\n", err)
+		return records
+	}
+
+	for _, sets := range v.ResourceRecordSets {
+		for _, record := range sets.ResourceRecordSet {
+			records = append(records, record)
+		}
+	}
+
+	if v.IsTruncated == true {
+		records = r.getResourceRecordSetsChunk(zone, v.NextRecordName, records)
+	}
+	return records
+}
+
+// GetResourceRecordSets returns all the resource record sets for the
+// given hosted zone.
+func (r *Route53) GetResourceRecordSets(zone string) []ResourceRecordSet {
+	var recordSet []ResourceRecordSet
+	records := r.getResourceRecordSetsChunk(zone, "", recordSet)
+
+	return records
 }
